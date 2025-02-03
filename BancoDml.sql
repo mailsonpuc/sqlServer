38270abe2c30751b8f06695dee03ab991c46ba5a
mailsonpuc <mailson.costa@proton.me>
2025-02-03 07:21:48 -0300

    Update BancoDml.sql

--- BancoDml.sql
+++ BancoDml.sql
@@ -29,7 +29,25 @@ INSERT INTO Curso (Id, Nome, CategoriaId) VALUES (
 
 GO
 
+--Inserindo dados na tabela ProgressoCurso
+INSERT INTO ProgressoCurso (AlunoId, CursoId, Progresso, UltimaAtualizacao) 
+VALUES 
+(
+    2, 1, 1, GETDATE()
+)
+GO
+
+--Inserido dados na tabela Aluno
+INSERT INTO Aluno (Id, Nome, Nascimento, Ativo, Email) 
+VALUES 
+(
+    1, 'Emily Costa', 1997-17-01, 1, 'E_Mily@gmail.com'
+)
+GO
 
 --Juntando duas tabelas e Selecionando tudo
 --                                          chaveEstrageira  =  chavePrimaria
 SELECT * FROM Curso INNER JOIN Categoria ON Curso.CategoriaId = Categoria.Id
+
+
+
