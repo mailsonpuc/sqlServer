84251466c90366993e1b1933d03d8042271cb278
mailsonpuc <mailson.costa@proton.me>
2025-09-06 20:30:18 -0300

    Update script.sql

--- script.sql
+++ script.sql
@@ -15,3 +15,20 @@ INSERT INTO base_de_dados.dbo.users
 (first_name, last_name, email, password_hash, created_at, updated_at)
 VALUES( 'Maria', 'silva', 'MS@mail.com', '40028922', getdate(),  getdate());
 
+
+-- seleciona apenas 4 dados
+ SELECT TOP 4 * FROM users u 
+
+-- usando BETWEEN
+SELECT *
+FROM users
+WHERE id  BETWEEN 1 and 10;
+
+-- usando delete
+ delete from users
+
+
+
+
+
+
